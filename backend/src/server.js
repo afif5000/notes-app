dcae88be769d70438f844941610761e8eb319758
afif500 <afifabdullah0109@gmail.com>
2025-09-23 20:16:19 +0700

    Notes App Project

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { testConnection } from "./config/db.js";
+import helloRoute from "./routes/helloRoute.js";
 
 const app = express();
 
 const port = 3000;
+
+//gunakan route
+app.use("/", helloRoute);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
