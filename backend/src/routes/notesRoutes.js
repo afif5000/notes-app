da491d758e769532281ed7101a020b0639cce6aa
afif500 <afifabdullah0109@gmail.com>
2025-09-24 20:16:59 +0700

    Notes App

---
+++ backend/src/routes/notesRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+} from "../handlers/notesHandler.js";
+
+const noteRouter = express.Router();
+
+noteRouter.get("/notes", getAllNotesHandler);
+noteRouter.post("/notes", addNoteHandler);
+noteRouter.get("/notes/:id", getNoteByIdHandler);
+
+export default noteRouter;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,17 @@
 import express from "express";
 import { testConnection } from "./config/db.js";
-import helloRoute from "./routes/helloRoute.js";
+import helloRouter from "./routes/helloRoute.js";
+import noteRouter from "./routes/notesRoutes.js";
 
 const app = express();
 
 const port = 3000;
 
 //gunakan route
-app.use("/", helloRoute);
+app.use(express.json());
+
+app.use(helloRouter);
+app.use(noteRouter);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
